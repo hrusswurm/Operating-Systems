04b1e106e2235a356e11403ef63ed6f604bbb48f
Hannah Russwurm <hrusswur@lxg-020.gaul.csd.uwo.ca>
2017-03-31 13:25:07 -0400

    setup

--- Assignment_3/asn3.c
+++ Assignment_3/asn3.c
@@ -9,35 +9,69 @@
 #include <unistd.h>
 
 
+int critical_section (int loop_limit, int who_called)
+{
+int flag;
 
+for (loop limit)
+{
+	printf (who_called, loop index);
+}
+printf (“returning to caller process x”); //x is the caller process
+return 0;
+}
 
 int main(){
 
 
 pid_t pid1, pid2, pid3, pid4, pid5, pid6, pid7, pid8, pid9;
 int status;
+int flag;
+
+int fd[2];
+int pipe = pipe(fd);
+
+//pipe error
+if (p1 < 0)
+ {
+    perror("pipe error");
+    exit(-1);
+ } 
+
 
 pid1 = fork();
 
 //fork error
-  if (pid1 < 0)
-  {
+if (pid1 < 0)
+{
   	perror("fork");
  	printf("main function: errno number is %d\n",errno);
 	exit(pid1);
-  }
+}
 
-if (pid1 != 0)
+//child1
+else if (pid1 == 0)
 {
-  	printf("From parent process %d Child_1 created with PID: %d\n", getpid(), pid1);
+
 }
 
 
 
+pid2 = fork();
 
+//fork error
+if (pid2 < 0)
+{
+        perror("fork");
+        printf("main function: errno number is %d\n",errno);
+        exit(pid1);
+}
 
+//child2
+else if (pid2 == 0)
+{
 
-
+}
 
 
 
