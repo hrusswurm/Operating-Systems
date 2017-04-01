2ef3d1cb3573f2d8c3c64712c10bf8df815e2eca
Hannah Russwurm <hrusswur@lxg-015.gaul.csd.uwo.ca>
2017-04-01 15:56:39 -0400

    progress

--- Assignment_3/asn3.c
+++ Assignment_3/asn3.c
@@ -9,15 +9,17 @@
 #include <unistd.h>
 
 
+//loop_limit is the number of iterations
 int critical_section (int loop_limit, int who_called)
 {
-	int flag;
-
-	for (loop limit)
+	//for (loop limit)
+	int loop_index = 0;
+	while (loop_index < loop_limit)
 	{
-		printf (who_called, loop index);
+		printf("%d called the critical region with index %d \n", getpid(),  loop_index);
+		loop_index++;
 	}
-	printf (“returning to caller process x”); //x is the caller process
+	printf("returning to caller process x"); //x is the caller process
 	return 0;
 }
 
@@ -26,49 +28,62 @@ int critical_section (int loop_limit, int who_called)
 
 int main()
 {
-
-
 	pid_t pid;
-	int status, flag;
+	int status;
 	int fd[2];
-	int pipe = pipe(fd);
-
-
-
-	for (int i = 0; i < 9; i++)
-	{
-		pid = fork();
-	}
-
-
+	int p1 = pipe(fd);
+	int i;int flag;
 
 	//pipe error
-	if (p1 < 0)
- 	{
-		perror("pipe error");
-    		exit(-1);
- 	} 
-
-
-	//fork error
-	if (pid < 0)
+        if (p1 < 0)
+        {
+                perror("pipe error");
+                exit(-1);
+        }
+	
+	int semaphore = 1;
+	write(fd[1], &semaphore, sizeof(semaphore));
+	for (i = 0; i < 9; i++)
 	{
-  		perror("fork");
- 		printf("main function: errno number is %d\n",errno);
-		exit(pid1);
+		pid = fork();
+		if(pid == 0)
+		{
+			break;
+		}
+		//fork error
+	        if (pid < 0)
+       	 	{
+        	        perror("fork");
+                	printf("main function: errno number is %d\n",errno);
+        	}
 	}
 
-	//child
-	else if (pid == 0)
+	int goThrough = 1;	
+	while (goThrough = 1)
 	{
-		//enter critical section
+		wait(NULL);
+		//printf("I'm waiting %d\n", getpid());
+		read(fd[0], &flag, sizeof(flag));
+		flag--;
+		write(fd[1], &flag, sizeof(flag));
+
+		if (flag == 0)
+		{
+			//enter
+			critical_section (1, getpid());
+			
+			flag++;
+			write(fd[1], &flag, sizeof(flag));
+			break;
+			
+		}
+		
 	}
 
+	printf("I'm done %d\n", getpid());
 
 
-
-
-
+	wait(NULL);
 	return 0;
 }
 
