218c60a7cb535d2de64d884ed3cb0870f696327a
Hannah Russwurm <hrusswur@lxg-020.gaul.csd.uwo.ca>
2017-04-04 13:40:06 -0400

    Code comments and cleanup

--- Assignment_3/asn3.c
+++ Assignment_3/asn3.c
@@ -1,90 +1,96 @@
 
 
-#include <sys/types.h> 
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
-#include <unistd.h>
 
-
+//This is the critical region method
 //loop_limit is the number of iterations
+//who_called is the process that called the critical_section
 int critical_section (int loop_limit, int who_called)
 {
-	//for (loop limit)
 	int loop_index = 0;
 	while (loop_index < loop_limit)
 	{
-		printf("%d called the critical region with index %d \n", getpid(),  loop_index);
+		printf("%d called the critical region with index %d\n", getpid(),  loop_index);
 		loop_index++;
 	}
-	printf("returning to caller process x"); //x is the caller process
+	printf("Returning to caller process %d\n", getpid()); //x is the caller process
 	return 0;
 }
 
 
 
-
+//main method
 int main()
 {
-	pid_t pid;
-	int status;
-	int fd[2];
-	int p1 = pipe(fd);
-	int i;int flag;
+	pid_t pid;		//process id
+	int fd[2];		//name of pipe
+	int p1 = pipe(fd);	//create the pipe
+	int i, flag;		//useful variables
 
-	//pipe error
+	//Handle pipe error
         if (p1 < 0)
         {
                 perror("pipe error");
                 exit(-1);
         }
 	
+	//Create semaphore in pipe
 	int semaphore = 1;
 	write(fd[1], &semaphore, sizeof(semaphore));
+
+	//Create multiple processes using fork()
 	for (i = 0; i < 9; i++)
 	{
 		pid = fork();
+		//If child, exit this loop so only parent creates processes
 		if(pid == 0)
 		{
 			break;
 		}
-		//fork error
+
+		//Handle fork error
 	        if (pid < 0)
        	 	{
         	        perror("fork");
                 	printf("main function: errno number is %d\n",errno);
         	}
 	}
-
+	
+	
 	int goThrough = 1;	
 	while (goThrough = 1)
-	{
+	{	
+		//wait ensures only one process at a time
 		wait(NULL);
-		//printf("I'm waiting %d\n", getpid());
+		
+		//Processes read from pipe and adjusts semaphore in pipe
 		read(fd[0], &flag, sizeof(flag));
 		flag--;
 		write(fd[1], &flag, sizeof(flag));
 
+		//enter critical section if semaphore is 0
 		if (flag == 0)
 		{
-			//enter
+			//call critical section method
 			critical_section (20, getpid());
 			
+			//readjust semaphore in pipe to indicate completion to the other processes
 			flag++;
 			write(fd[1], &flag, sizeof(flag));
+			//once done, break out so we do not revisit critical section
 			break;
 			
 		}
 		
 	}
+	//Indicate completion to the screen
+	printf("I'm done %d\n\n", getpid());
 
-	printf("I'm done %d\n", getpid());
-
-
-	wait(NULL);
 	return 0;
 }
 
